d522e50db7b9f0371f47f2512c6ee3eecd9f7da3
Giovanni Washel <giowashel@gmail.com>
2023-06-29 15:22:45 -0400

    finished part of search bar

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
       {
-        //<SearchBar/>
-        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm} />
+        <SearchBar/>
+        //currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm} />
 
 
       }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./SearchBar.css";
 import {FaSearch} from "react-icons/fa";
-
 export const SearchBar = () => {
 
     return(
         <div className="search-wrapper">
             <FaSearch id="search-icon" />
-            <input placeholder="Enter a recipe..." type="text" />
+            <input className="search-input"placeholder="Enter a recipe..." type="text" />
         </div>
 
     );
