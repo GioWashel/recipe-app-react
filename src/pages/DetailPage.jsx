e106bc22b60ee9b9452177af64798b00fed11cef
Djezzar Moncef <djezzarmonsef@gmail.com>
2023-07-01 20:43:08 +0100

    details page code improvements

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,45 +1,21 @@
 import "./Detailpage.css";
 
-export const DetailPage = () => {
-  // testing data ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-  const recipeTitle = `Spaghetti Carbonara`;
-  const recipeImageURL = `https://th.bing.com/th/id/R.a0246b9032f0cd5338d8391642d8bcf1?rik=B8NK%2biKQg0zVmg&riu=http%3a%2f%2fwww.casaacorte.com%2fwp-content%2fuploads%2f2014%2f01%2fCarbonara-pepper-web.jpg&ehk=cJqi017Y10hmUui9pXcu3tNrn%2blKjkDpSwktNMz0cx4%3d&risl=&pid=ImgRaw&r=0`;
-  const ingredients = [
-    `1 pound spaghetti.`,
-    `6 ounces pancetta or bacon, diced.`,
-    `4 large eggs.`,
-    `1 cup freshly grated Parmesan cheese.`,
-    `1/4 cup chopped fresh parsley.`,
-    `Salt and freshly ground black pepper to taste.`,
-  ];
-  const serving = `4 servings`;
-  const instructions = [
-    `Cook the spaghetti according to package instructions until al dente. Reserve 1 cup of the pasta cooking water, then drain the spaghetti.`,
-    `In a large skillet, cook the pancetta or bacon over medium heat until crisp. Remove the pancetta or bacon with a slotted spoon and drain on paper towels.`,
-    `In a large bowl, whisk together the eggs, Parmesan cheese, and chopped parsley.`,
-    `Add the hot drained spaghetti to the bowl with the egg mixture and toss well to coat.`,
-    `Add the cooked pancetta or bacon to the bowl and toss again.`,
-    `If the pasta seems too dry, add a little of the reserved pasta cooking water to moisten it.`,
-    `Season the pasta with salt and freshly ground black pepper to taste.`,
-    `Serve the spaghetti carbonara hot, garnished with additional Parmesan cheese and chopped parsley if desired.`,
-  ];
-  //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+export const DetailPage = ({recipe}) => {
   return (
     <div className="recipe">
       <div className="img-container">
-        <img src={recipeImageURL} alt={recipeTitle} />
+        <img src={recipe.imageURL} alt={recipe.name} />
       </div>
       <div className="details">
-        <h2 className="title">{recipeTitle}</h2>
+        <h2 className="title">{recipe.name}</h2>
         <div className="serving">
           <h3>Serving</h3>
-          <p>{serving}</p>
+          <p>{recipe.serving}</p>
         </div>
         <div className="ingredients">
           <h3>Ingredients</h3>
           <ul>
-            {ingredients.map((item, index) => (
+            {recipe.ingredients.map((item, index) => (
               <li key={index}>
                 <label htmlFor={`ingredient-${index}`}>
                   <p>{item}</p>
@@ -57,7 +33,7 @@ export const DetailPage = () => {
         <div className="instructions">
           <h3>Instructions</h3>
           <ul>
-            {instructions.map((item, index) => (
+            {recipe.instructions.map((item, index) => (
               <li key={index}>
                 <label htmlFor={`instruction-${index}`}>
                   <p>{item}</p>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,35 @@
 import React from "react";
-import './HomePage.css';
-import {Link} from 'react-router-dom';
+import "./HomePage.css";
+import { Link } from "react-router-dom";
 import { SearchBar } from "../components/SearchBar";
 
 export const Home = () => {
-    return (
-        <div className="home">
-            <SearchBar />
-            <div className="links">
-                <h3>Temporary Links for dev purposes</h3>
-                
-                <Link to="/" >Home <span>You are here</span></Link>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-                <Link to="/profile">Profile<span>Assigned to coronel</span></Link> 
-                <Link to="/details">Show Recipe in Detail<span>Assigned</span></Link> 
-                <Link to="/explore">Explore Recipes<span>Unassigned</span></Link> 
-                <Link to="/create">Create a New Recipe<span>Assigned, Not started</span></Link> 
-            </div>
-        </div>
-    )
+  return (
+    <div className="home">
+      <SearchBar />
+      <div className="links">
+        <h3>Temporary Links for dev purposes</h3>
+
+        <Link to="/">
+          Home <span>You are here</span>
+        </Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/profile">
+          Profile <span>Assigned to coronel</span>
+        </Link>
+        <Link to="/details">
+          Show Recipe in Detail <span>Assigned</span>
+        </Link>
+        <Link to="/explore">
+          Explore Recipes <span>Unassigned</span>
+        </Link>
+        <Link to="/create">
+          Create a New Recipe <span>Assigned, Not started</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
