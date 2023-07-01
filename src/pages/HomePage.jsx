8014faf5e1e9a83ea95d18365dd13ce550e21e86
Djezzar Moncef <djezzarmonsef@gmail.com>
2023-07-01 23:11:49 +0100

    fixed the conflict issue

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,32 +1,18 @@
 import React from "react";
 import "./HomePage.css";
-import { Link } from "react-router-dom";
-import { SearchBar } from "../components/SearchBar";
+import { Login } from "./Login";
 
+import { SearchBar } from "./SearchBar";
 export const Home = () => {
   return (
     <div className="home">
-      <SearchBar />
-      <div className="links">
-        <h3>Temporary Links for dev purposes</h3>
-
-        <Link to="/">
-          Home <span>You are here</span>
-        </Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/profile">
-          Profile <span>Assigned to coronel</span>
-        </Link>
-        <Link to="/details">
-          Show Recipe in Detail <span>Assigned</span>
-        </Link>
-        <Link to="/explore">
-          Explore Recipes <span>Unassigned</span>
-        </Link>
-        <Link to="/create">
-          Create a New Recipe <span>Assigned, Not started</span>
-        </Link>
+      <div className="left-home">
+        <h1>So, what are you planning to eat?</h1>
+        <h1>We have got you covered!</h1>
+        <SearchBar />
+      </div>
+      <div className="right-home">
+        <Login />
       </div>
     </div>
   );
