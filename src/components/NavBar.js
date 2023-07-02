e8a347139a5bd3a237a9aa9e44be26ccbdf73e54
Djezzar Moncef <djezzarmonsef@gmail.com>
2023-07-02 13:51:31 +0100

    added the log to the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,35 @@
 import React from "react";
-import logo from "./logo.jpg";
+import logo from "./first-color-logo.png";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 export const NavBar = () => {
-    return(
-        <>
-        <div className="navbar-container">
-            <nav>
-                <ul class="nav-links">
-                    <li><Link to="/home" id="home-link" className="links">Home</Link> </li>
-                    <li><Link to="/explore" id="explore-link" className="links">Explore</Link> </li>
-                    <li> <Link to="/create" id="create-link" className="links">Create</Link></li>
-
-                </ul>
-            </nav>
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="logo-container">
+          <img id="logo" src={logo} alt="logo" />
         </div>
-        </>
-    );
-}
+        <nav>
+          <ul class="nav-links">
+            <li>
+              <Link to="/home" id="home-link" className="links">
+                Home
+              </Link>{" "}
+            </li>
+            <li>
+              <Link to="/explore" id="explore-link" className="links">
+                Explore
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/create" id="create-link" className="links">
+                Create
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
