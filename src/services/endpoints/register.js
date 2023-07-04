ebb110abbd903084fcae125a44ebf89fe4aed3a7
Carlos <92417699+cr2551@users.noreply.github.com>
2023-07-04 01:15:37 -0400

    moved getCookies() function to utils folder

--- src/services/endpoints/register.js
+++ src/services/endpoints/register.js
@@ -7,22 +7,6 @@ export const register = (data) => {
 }
 
 
-export function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        let cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            let cookie = cookies[i].trim()
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break
-            }
-        }
-    }
-    return cookieValue
-}
-
-
 // const sendLogin = axios.create({
 //     baseURL: "http://localhost:8000",
 //     timeout: 5000,
