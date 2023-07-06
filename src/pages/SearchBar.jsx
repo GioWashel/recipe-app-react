50d75348b5e39b39c69e6abe495a2dfef6e774d7
Carlos <92417699+cr2551@users.noreply.github.com>
2023-07-06 02:01:54 -0400

    connect big search bar to api

--- src/pages/SearchBar.jsx
+++ src/pages/SearchBar.jsx
@@ -1,11 +1,21 @@
 import "./SearchBar.css";
 import {FaSearch} from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+
+
 export const SearchBar = () => {
+    
+    const navigate = useNavigate();
+    function search(e, value) {
+        if (e.key === 'Enter') navigate(`/search/${value}`);
+    }
 
     return(
         <div className="search-wrapper">
             <FaSearch id="search-icon" />
-            <input className="search-input"placeholder="Find a recipe..." type="text" />
+            <input className="search-input"placeholder="Find a recipe..." type="text"
+                onKeyDown={e => search(e, e.target.value)}
+            ></input>
         </div>
 
     );
--- src/services/utils/request.js
+++ src/services/utils/request.js
@@ -1,4 +1,5 @@
 import api from "../api";
+
 export async function get(endpoint) {
   try {
     const response = await api.get(endpoint);
