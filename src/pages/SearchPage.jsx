bb92ced523c053bd14b5cbcbe14b8c083e5a2305
cr2551 <92417699+cr2551@users.noreply.github.com>
2023-07-13 22:15:22 -0400

    fix: search page not displaying results

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { ExplorePage } from "./ExplorePage";
 import { RecipeCard } from "../components/RecipeCard";
 import { searchRecipe } from "../services/endpoints/recipes";
 import "./SearchPage.css";
@@ -8,13 +7,15 @@ export const SearchPage = () => {
     const [recipes, setRecipes] = useState([]);
     const {query} = useParams();
     const [isloading, setIsLoading] = useState(true);
-    const [isRecipeFound, setIsRecipeFound ] = useState("");
+    const [isRecipeFound, setIsRecipeFound ] = useState(false);
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = searchRecipe(query);
-                if (recipes !== []) setRecipes(res);
-                else setIsRecipeFound(false);
+                const res = await searchRecipe(query);
+                if (recipes !== []) {
+                    setRecipes(res);
+                    setIsRecipeFound(true);
+                }
             } catch (error) {
                 console.error("error fetching recipes", error);
             }
